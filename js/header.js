4b0c82568670ece07499a69b7fa7cf112cae9c08
quanghieu265 <hieu26594@gmail.com>
2021-07-12 19:23:08 +0700

    add page product detail

---
+++ js/header.js
@@ -0,0 +1,50 @@
+// Header 
+// pop-up add to cart
+$(".toast-close").on("click", function () {
+    $('.toast').toast('hide')
+})
+
+$(".button-cart").on("click", function () {
+    $('.toast').toast('show')
+})
+
+// Sidebar mobi
+$(".fa-bars").on("click", function () {
+    $(".side-bar").addClass("sidebar-show")
+})
+
+$(".fa-times").on("click", function () {
+    $(".side-bar").removeClass("sidebar-show")
+})
+
+// Search-mobi
+$(".fa-search").on("click", function () {
+    $(".search-mobi-box").toggleClass("search-show")
+})
+
+//   Nav-scroll
+let lastScrollTop = 0;
+let firstCenter = ($(window).innerHeight() + $(this).scrollTop()) / 2;;
+$(window).scroll(function (event) {
+    let st = $(this).scrollTop();
+    //Nav event
+    if (st > firstCenter) {
+        if (st > lastScrollTop) {
+            // Scroll down
+            $(".header").addClass("hide-nav");
+        } else {
+            // Scroll up
+            $(".header").removeClass("hide-nav");
+        }
+        lastScrollTop = st;
+    }
+})
+
+$(document).ready(function () {
+    // After browser render carousel, trigger event "Preview img hover"
+    $('.preview-img > img').hover(function () {
+      let eq = $(".preview-img > img").index(this)
+      console.log(eq);
+      $(`.fade-in:eq(${eq})`).toggleClass("fade-out");
+    })
+  });
---
+++ js/main.js
@@ -0,0 +1,98 @@
+// Swipe for bootstrap carousel
+$(".carousel").swipe({
+  swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+  },
+  allowPageScroll: "vertical"
+});
+
+
+// event scroll
+// let firstCenter=($(window).innerHeight() + $(this).scrollTop()) / 2;;
+let positionTypeProduct = "";
+let viewportCenter = "";
+$(window).scroll(function (event) {
+  // Parameter
+  positionTypeProduct = $(".product-type").position()
+  viewportCenter = ($(window).innerHeight() / 2) + $(this).scrollTop()
+  let positionNews = $(".news").position()
+  let positionBanner = $(".banner-sale").position()
+  let positionPolicy = $(".policy").position()
+
+  // Type Product
+  if (viewportCenter > positionTypeProduct.top) {
+    $(".product-type-card").addClass("opacity-show");
+  } else {
+    $(".product-type-card").removeClass("opacity-show");
+  }
+  //banner sale event
+  if (viewportCenter > positionBanner.top) {
+    $(".sale-img").addClass("opacity-show");
+    $(".banner-text-sale").addClass("text-show");
+  } else {
+    $(".sale-img").removeClass("opacity-show");
+    $(".banner-text-sale").removeClass("text-show");
+  }
+  // News
+  if (viewportCenter > positionNews.top) {
+    $(".news-hide").addClass("news-show");
+  } else {
+    $(".news-hide").removeClass("news-show");
+  }
+  //Policy
+  if (viewportCenter > positionPolicy.top) {
+    $(".policy-content").addClass("opacity-show");
+  } else {
+    $(".policy-content").removeClass("opacity-show");
+  }
+
+});
+
+// -----------------------------------------------------------------------------------------------
+
+// Own Carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 10,
+    loop: true,
+    dots: true,
+    dotsEach: 2,
+    items: 4,
+    nav: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      1: {
+        items: 1,
+        nav: false
+      },
+      576: {
+        items: 2,
+        nav: false
+      },
+      768: {
+        items: 3,
+        nav: true,
+      },
+      968: {
+        items: 4,
+        nav: true,
+      }
+    }
+  });
+
+  positionTypeProduct = $(".product-type").position();
+  if (firstCenter > positionTypeProduct.top) {
+    $(".product-type-card").addClass("opacity-show");
+  }
+
+  // After browser render carousel, trigger event "Preview img hover"
+  $('.preview-img > img').hover(function () {
+    let eq = $(".preview-img > img").index(this)
+    console.log(eq);
+    $(`.fade-in:eq(${eq})`).toggleClass("fade-out");
+  })
+});
